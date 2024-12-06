af5ab6aabb283d0d427753b70eb618e9bc15e8c1
user <sotvoldiyevazamat193@gmail.com>
2024-12-06 22:31:16 +0500

    Remoasckzsmdkasdkls

--- conf/settings.py
+++ conf/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','t1murov.uz', 'localhost', '127.0.0.1', '64.227.75.226']
 # settings.py
 
 
